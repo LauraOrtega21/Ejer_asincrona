0dd69b938432d3e9b1ed5471425989054892d2b7
LauraOrtega <laurapaolao2110@gmail.com>
2024-05-23 11:13:54 -0500

    Avance2

---
+++ ejer19.js
@@ -0,0 +1,14 @@
+let suma = 0;
+
+async function datos() {
+  const response = await fetch("ejercicio19json.json");
+  const datos = await response.json();
+  let datoss = Object.values(datos);
+  for (const item of datoss) {
+    suma += item;
+  }
+  console.log(suma)
+ 
+}
+
+datos();
